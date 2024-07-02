a2fa51edefd7c4f192eb28b46b6b35515948bc2a
wowyomad <vadimsun8837@gmail.com>
2024-07-02 19:22:36 +0300

    oops

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import ReactDOM from 'react-dom/client'
-import { App } from './App.1.tsx';
+import { App } from './App.tsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 
 
