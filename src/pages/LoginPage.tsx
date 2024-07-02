18233f9bce5964d5b1a141e418eb4bbd1635a68b
wowyomad <vadimsun8837@gmail.com>
2024-07-03 02:14:20 +0300

    Refactor
    Removed unnecessary div element

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -9,8 +9,8 @@ interface LoginPageProps {
     onLoginSuccess: (auth: AuthenticationResponse) => Promise<void>;
 }
 
-const LoginPage: React.FC<LoginPageProps> = ({onLoginSuccess}) => {
-    const [formData, setFormData] = useState<UserLogin>({email: '', password: ''})
+const LoginPage: React.FC<LoginPageProps> = ({ onLoginSuccess }) => {
+    const [formData, setFormData] = useState<UserLogin>({ email: '', password: '' })
     const [error, setError] = useState<string>('')
 
     const navigate = useNavigate()
@@ -47,64 +47,61 @@ const LoginPage: React.FC<LoginPageProps> = ({onLoginSuccess}) => {
     return (
         <div className="container-sm mt-5">
             <div className="row justify-content-center">
-                <div className="col-md-5">
-                    <form className="shadow-3-strong p-5 mb-5 rounded" action="#!">
-                        <div className="form-floating mb-4">
-                            <input
-                                type="email"
-                                className="form-control"
-                                id="email"
-                                name="email"
-                                placeholder="name@example.com"
-                                value={formData.email}
-                                onChange={handleChange}
-                            />
-                            <label htmlFor="email">Email address</label>
-                        </div>
+                <div className="col-md-5 shadow-3-strong p-5 mb-5 rounded">
+                    <div className="form-floating mb-4">
+                        <input
+                            type="email"
+                            className="form-control"
+                            id="email"
+                            name="email"
+                            placeholder="name@example.com"
+                            value={formData.email}
+                            onChange={handleChange}
+                        />
+                        <label htmlFor="email">Email address</label>
+                    </div>
 
-                        <div className="form-floating mb-4">
-                            <input
-                                type="password"
-                                className="form-control"
-                                id="password"
-                                name="password"
-                                placeholder="Password"
-                                value={formData.password}
-                                onChange={handleChange}
-                            />
-                            <label htmlFor="password">Password</label>
-                        </div>
+                    <div className="form-floating mb-4">
+                        <input
+                            type="password"
+                            className="form-control"
+                            id="password"
+                            name="password"
+                            placeholder="Password"
+                            value={formData.password}
+                            onChange={handleChange}
+                        />
+                        <label htmlFor="password">Password</label>
+                    </div>
 
-                        <div className="row mb-4">
-                            <div className="col d-flex justify-content-center">
-                                <div className="form-check">
-                                    <input className="form-check-input" type="checkbox" value="" id="remember" defaultChecked />
-                                    <label className="form-check-label" htmlFor="remember">
-                                        Remember me
-                                    </label>
-                                </div>
-                            </div>
-
-                            <div className="col">
-                                <a href="#!">Forgot password?</a>
+                    <div className="row mb-4">
+                        <div className="col d-flex justify-content-center">
+                            <div className="form-check">
+                                <input className="form-check-input" type="checkbox" value="" id="remember" defaultChecked />
+                                <label className="form-check-label" htmlFor="remember">
+                                    Remember me
+                                </label>
                             </div>
                         </div>
-                        <div className="d-flex justify-content-center mb-4">
-                            <button onClick={handleSignin} type="button" className="btn btn-primary btn-lg ps-5 pe-5">
-                                Sign in
-                            </button>
+
+                        <div className="col">
+                            <a href="#!">Forgot password?</a>
                         </div>
-                        <div className="text-center mb-5">
-                            <span>
-                                Not registered? <button onClick={() => navigate('/signup')} type="button" className="btn btn-link">Register</button>
-                            </span>
-                        </div>
-                        <div className="text-danger">
-                            {error}
-                        </div>
-                    </form>
+                    </div>
+                    <div className="d-flex justify-content-center mb-4">
+                        <button onClick={handleSignin} type="button" className="btn btn-primary btn-lg ps-5 pe-5">
+                            Sign in
+                        </button>
+                    </div>
+                    <div className="text-center mb-5">
+                        <span>
+                            Not registered? <button onClick={() => navigate('/signup')} type="button" className="btn btn-link">Register</button>
+                        </span>
+                    </div>
+                    <div className="text-danger">
+                        {error}
+                    </div>
                 </div>
-
             </div>
         </div>
     );
